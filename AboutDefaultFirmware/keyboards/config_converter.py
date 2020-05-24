5f4de857bac60e2e3758929be5a2ac033c79cafc
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2020-05-25 06:34:18 +0900

    Fix ergo42 config

--- AboutDefaultFirmware/keyboards/config_converter.py
+++ AboutDefaultFirmware/keyboards/config_converter.py
@@ -19,7 +19,8 @@ def green(string):
 
 
 class ConfigConverter:
-    PIN_TABLE = {'D3': 1, 'D2': 2, 'D1': 5, 'D0': 6, 'D4': 7, 'C6': 8, 'D7': 9, 'E6': 10, 'B4': 11, 'B5': 12, 'B6': 13, 'B2': 14, 'B3': 15, 'B1': 16, 'F7': 17, 'F6': 18, 'F5': 19, 'F4': 20}
+    PIN_TABLE = {'D3': 1, 'D2': 2, 'D1': 5, 'D0': 6, 'D4': 7, 'C6': 8, 'D7': 9, 'E6': 10, 'B4': 11,
+                 'B5': 12, 'B6': 13, 'B2': 14, 'B3': 15, 'B1': 16, 'F7': 17, 'F6': 18, 'F5': 19, 'F4': 20}
 
     def __init__(self):
         self.is_split = False
@@ -49,7 +50,8 @@ class ConfigConverter:
         line = 1
         for string in lines:
             string = self.remove_comment(string)
-            layout_name = re.match(r'^\s*#\s*define\s+LAYOUT[^\r\n\t\f\v \\]*\s*', string)
+            layout_name = re.match(
+                r'^\s*#\s*define\s+LAYOUT[^\r\n\t\f\v \\]*\s*', string)
             if layout_name is not None:
                 layout_name = layout_name.group().split("#define")[1]
                 layout_name = layout_name.replace('(', '')
@@ -57,7 +59,8 @@ class ConfigConverter:
                 file_name = f.name
                 if 'keyboards/' in file_name:
                     file_name = file_name.split('keyboards/')[1]
-                self.layout_macro.append({"name": layout_name, "file": file_name, "line": line})
+                self.layout_macro.append(
+                    {"name": layout_name, "file": file_name, "line": line})
             line = line + 1
 
     def set_layout_index(self, idx):
@@ -70,7 +73,8 @@ class ConfigConverter:
     def print_layout_list(self):
         index = 0
         for layout in self.layout_macro:
-            print(f'{index}: {layout["name"]}\t\t{layout["file"]}:{layout["line"]}')
+            print(
+                f'{index}: {layout["name"]}\t\t{layout["file"]}:{layout["line"]}')
             index = index + 1
 
     def convert_pindef(self, arg):
@@ -93,7 +97,7 @@ class ConfigConverter:
         return pin_str
 
     def convert_layout(self, arg):
-        # remove "#define LAYOUTxxx(" from arg 
+        # remove "#define LAYOUTxxx(" from arg
         str2 = re.sub(r'(\(|{|})', '', arg)
         str2 = str2.replace('#define', '')
         str2 = str2.replace(self.search_layout, '')
@@ -115,7 +119,7 @@ class ConfigConverter:
 
         array = array.split(',')
 
-        # pick up a element from LAYOUT macro arguments and find it from array 
+        # pick up a element from LAYOUT macro arguments and find it from array
         layout = []
         for row in place.split('\n'):
             if row == '':
@@ -134,7 +138,8 @@ class ConfigConverter:
         return layout_str
 
     def waring_multiple_definition(self, symbol, f, line):
-        print(yellow('warning') + f': multiple definition of {symbol} in {f.name}:{line}. Previous definition is used.')
+        print(yellow('warning') +
+              f': multiple definition of {symbol} in {f.name}:{line}. Previous definition is used.')
 
     def remove_comment(self, arg):
         return arg.split('//')[0]
@@ -148,25 +153,32 @@ class ConfigConverter:
             line = line + 1
             string = self.remove_comment(string)
             if 'VENDOR_ID' in string:
-                self.vid = string.split('VENDOR_ID')[1].replace('\n', '').strip()
+                self.vid = string.split('VENDOR_ID')[
+                    1].replace('\n', '').strip()
             elif 'PRODUCT_ID' in string:
-                self.pid = string.split('PRODUCT_ID')[1].replace('\n', '').strip()
+                self.pid = string.split('PRODUCT_ID')[
+                    1].replace('\n', '').strip()
             elif 'MANUFACTURER' in string:
-                self.manufacture = string.split('MANUFACTURER')[1].replace('\n', '').strip()
+                self.manufacture = string.split('MANUFACTURER')[
+                    1].replace('\n', '').strip()
             elif 'PRODUCT' in string:
-                self.name = string.split('PRODUCT')[1].replace('\n', '').strip()
+                self.name = string.split('PRODUCT')[
+                    1].replace('\n', '').strip()
             elif 'DESCRIPTION' in string:
-                self.description = string.split('DESCRIPTION')[1].replace('\n', '').strip()
+                self.description = string.split('DESCRIPTION')[
+                    1].replace('\n', '').strip()
             elif re.match(r'\s*#\s*define\s+MATRIX_ROWS\s', string):
                 if self.row_num != '':
                     self.waring_multiple_definition('MATRIX_ROWS', f, line)
                 else:
-                    self.row_num = string.split('MATRIX_ROWS')[1].replace('\n', '').strip()
+                    self.row_num = string.split('MATRIX_ROWS')[
+                        1].replace('\n', '').strip()
             elif re.match(r'\s*#\s*define\s+MATRIX_COLS\s', string):
                 if self.col_num != '':
                     self.waring_multiple_definition('MATRIX_COLS', f, line)
                 else:
-                    self.col_num = string.split('MATRIX_COLS')[1].replace('\n', '').strip()
+                    self.col_num = string.split('MATRIX_COLS')[
+                        1].replace('\n', '').strip()
             elif re.match(r'\s*#\s*define\s+MATRIX_ROW_PINS\s', string):
                 if self.row_pins != '':
                     self.waring_multiple_definition('MATRIX_ROW_PINS', f, line)
@@ -179,19 +191,22 @@ class ConfigConverter:
                     self.col_pins = self.convert_pindef(string)
             elif re.match(r'\s*#\s*define\s+MATRIX_ROW_PINS_RIGHT\s', string):
                 if self.row_pins_right != '':
-                    self.waring_multiple_definition('MATRIX_ROW_PINS_RIGHT', f, line)
+                    self.waring_multiple_definition(
+                        'MATRIX_ROW_PINS_RIGHT', f, line)
                 else:
                     self.row_pins_right = self.convert_pindef(string)
             elif re.match(r'\s*#\s*define\s+MATRIX_COL_PINS_RIGHT\s', string):
                 if self.col_pins_right != '':
-                    self.waring_multiple_definition('MATRIX_COL_PINS_RIGHT', f, line)
+                    self.waring_multiple_definition(
+                        'MATRIX_COL_PINS_RIGHT', f, line)
                 else:
                     self.col_pins_right = self.convert_pindef(string)
             elif re.match(r'\s*#\s*define\s+DIODE_DIRECTION\s', string):
                 if self.diode_direction != '':
                     self.waring_multiple_definition('DIODE_DIRECTION', f, line)
                 else:
-                    direction = string.split('DIODE_DIRECTION')[1].replace('\n', '').strip()
+                    direction = string.split('DIODE_DIRECTION')[
+                        1].replace('\n', '').strip()
                     if direction == 'ROW2COL':
                         self.diode_direction = 1
                     else:
@@ -200,16 +215,19 @@ class ConfigConverter:
                 if self.led_pin != '':
                     self.waring_multiple_definition('RGB_DI_PIN', f, line)
                 else:
-                    pin = string.split('RGB_DI_PIN')[1].replace('\n', '').strip()
+                    pin = string.split('RGB_DI_PIN')[
+                        1].replace('\n', '').strip()
                     self.led_pin = self.PIN_TABLE[pin]
             elif re.match(r'\s*#\s*define\s+RGBLED_NUM\s', string):
                 if self.led_num != '':
                     self.waring_multiple_definition('RGBLED_NUM', f, line)
                 else:
-                    self.led_num = string.split('RGBLED_NUM')[1].replace('\n', '').strip()
+                    self.led_num = string.split('RGBLED_NUM')[
+                        1].replace('\n', '').strip()
             elif '#define ' + self.search_layout + '(' in string:
                 if self.layout != '':
-                    self.waring_multiple_definition(self.search_layout, f, line)
+                    self.waring_multiple_definition(
+                        self.search_layout, f, line)
                 else:
                     layout_detect = True
 
@@ -246,10 +264,11 @@ class ConfigConverter:
 
         if (int(self.col_num) != len(self.col_pins.split(',')) or int(self.row_num) != len(self.row_pins.split(','))):
             self.is_split = True
-            print('This keyboard is spilit keyboard')
+            print('This keyboard is split keyboard')
 
         if (int(self.col_num) != len(self.col_pins.split(',')) and int(self.row_num) != len(self.row_pins.split(','))):
-            print(yellow('warning') + ': MATRIX len and PINS len do not match in both row and col.')
+            print(yellow('warning') +
+                  ': MATRIX len and PINS len do not match in both row and col.')
 
         return res
 
@@ -277,7 +296,8 @@ class ConfigConverter:
 if __name__ == '__main__':
     args = sys.argv
     if len(args) == 1:
-        print(('Usage : config_converter.py [TARGET]\n' ' ex: ./config_covnerte.py ~/qmk_firmware/keyboards/helix/rev2\n'))
+        print(
+            ('Usage : config_converter.py [TARGET]\n' ' ex: ./config_covnerte.py ~/qmk_firmware/keyboards/helix/rev2\n'))
         sys.exit()
 
     config = ConfigConverter()
@@ -348,8 +368,10 @@ if __name__ == '__main__':
     with open(config_name, 'w') as f:
         f.write(
             config_file_format.format(
-                config.vid, config.pid, config.name, config.manufacture, config.description, config.row_num, config.col_num, len(config.row_pins.split(',')), len(config.col_pins.split(',')), config.diode_direction, config.row_pins, config.col_pins,
-                config.layout.replace('\n', '\n\t\t\t'), mode, config.led_pin, config.led_num
+                config.vid, config.pid, config.name, config.manufacture, config.description, config.row_num, config.col_num, len(
+                    config.row_pins.split(',')), len(config.col_pins.split(',')), config.diode_direction, config.row_pins, config.col_pins,
+                config.layout.replace(
+                    '\n', '\n\t\t\t'), mode, config.led_pin, config.led_num
             )
         )
 
@@ -363,8 +385,10 @@ if __name__ == '__main__':
     with open(config_name, 'w') as f:
         f.write(
             config_file_format.replace('"is_left_hand":1', '"is_left_hand":0').format(
-                config.vid, config.pid, config.name, config.manufacture, config.description, config.row_num, config.col_num, len(config.row_pins_right.split(',')), len(config.col_pins_right.split(',')), config.diode_direction, config.row_pins_right,
-                config.col_pins_right, config.layout.replace('\n', '\n\t\t\t'), mode, config.led_pin, config.led_num
+                config.vid, config.pid, config.name, config.manufacture, config.description, config.row_num, config.col_num, len(
+                    config.row_pins_right.split(',')), len(config.col_pins_right.split(',')), config.diode_direction, config.row_pins_right,
+                config.col_pins_right, config.layout.replace(
+                    '\n', '\n\t\t\t'), mode, config.led_pin, config.led_num
             )
         )
 
@@ -372,23 +396,26 @@ if __name__ == '__main__':
 
     config_name = config_name.replace('slave_right_', 'lpme_left_')
     lpme_row = ([int(r) for r in config.row_pins.strip('[]').split(',')])
-    lpme_row.extend([int(r) for r in config.row_pins_right.strip('[]').split(',')])
+    lpme_row.extend([int(r)
+                     for r in config.row_pins_right.strip('[]').split(',')])
     lpme_col = ([int(r) for r in config.col_pins.strip('[]').split(',')])
-    lpme_col.extend([int(r) for r in config.col_pins_right.strip('[]').split(',')])
+    lpme_col.extend([int(r)
+                     for r in config.col_pins_right.strip('[]').split(',')])
     mode = 'SINGLE'
     with open(config_name, 'w') as f:
         f.write(
             config_file_format.format(
                 config.vid, config.pid, config.name, config.manufacture,
                 config.description, config.row_num, config.col_num,
-                len(config.row_pins.split(',')), len(config.col_pins.split(',')),
+                len(config.row_pins.split(',')), len(
+                    config.col_pins.split(',')),
                 str(int(config.diode_direction) + 2),
                 str(lpme_row),
                 str(lpme_col),
                 config.layout.replace('\n', '\n\t\t\t'),
                 mode,
                 config.led_pin, config.led_num
-                )
-           )
+            )
+        )
 
     print(f' ** Output to {config_name} ** \n')
